b2ca12bf02dff86b1979ac2f4adb9c07f9e4d0fd
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-11 18:42:25 +0700

    Update PetService

--- src/main/java/com/helpet/service/pet/service/PetService.java
+++ src/main/java/com/helpet/service/pet/service/PetService.java
@@ -3,14 +3,15 @@ package com.helpet.service.pet.service;
 import com.helpet.exception.ConflictLocalizedException;
 import com.helpet.exception.ForbiddenLocalizedException;
 import com.helpet.exception.NotFoundLocalizedException;
+import com.helpet.service.pet.dto.request.CreatePetRequest;
+import com.helpet.service.pet.dto.request.UpdateMedicalCardRequest;
 import com.helpet.service.pet.service.error.ConflictLocalizedError;
-import com.helpet.service.pet.service.error.ForbiddenLocalizedError;
 import com.helpet.service.pet.service.error.NotFoundLocalizedError;
-import com.helpet.service.pet.store.model.*;
-import com.helpet.service.pet.store.repository.PetRepository;
-import com.helpet.service.pet.web.dto.request.AddPetToFamilyRequest;
-import com.helpet.service.pet.web.dto.request.CreatePetRequest;
-import com.helpet.service.pet.web.dto.request.UpdateMedicalCardRequest;
+import com.helpet.service.pet.storage.model.Account;
+import com.helpet.service.pet.storage.model.Pet;
+import com.helpet.service.pet.storage.model.PetCategory;
+import com.helpet.service.pet.storage.model.Species;
+import com.helpet.service.pet.storage.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,7 @@ import java.util.UUID;
 
 @Service
 public class PetService {
-    private final UserService userService;
-
-    private final FamilyService familyService;
+    private final AccountService accountService;
 
     private final PetCategoryService petCategoryService;
 
@@ -31,106 +30,78 @@ public class PetService {
     private final PetRepository petRepository;
 
     @Autowired
-    public PetService(UserService userService,
-                      FamilyService familyService,
+    public PetService(AccountService accountService,
                       PetCategoryService petCategoryService,
                       SpeciesService speciesService,
                       PetRepository petRepository) {
-        this.userService = userService;
-        this.familyService = familyService;
+        this.accountService = accountService;
         this.petCategoryService = petCategoryService;
         this.speciesService = speciesService;
         this.petRepository = petRepository;
     }
 
-    public Pet getPet(UUID petId) throws NotFoundLocalizedException {
-        return petRepository.findPetById(petId).orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.PET_DOES_NOT_EXIST));
-    }
-
-    public boolean userIsRelatedToPet(UUID userId, UUID petId) throws NotFoundLocalizedException {
-        if (!userService.userExists(userId)) {
-            throw new NotFoundLocalizedException(NotFoundLocalizedError.USER_DOES_NOT_EXIST);
+    public boolean userIsAssociatedWithPet(UUID userId, UUID petId) throws NotFoundLocalizedException {
+        if (!accountService.accountExists(userId)) {
+            throw new NotFoundLocalizedException(NotFoundLocalizedError.ACCOUNT_DOES_NOT_EXIST);
         }
 
-        Pet pet = getPet(petId);
-
-        Family family = pet.getFamily();
-        if (Objects.isNull(family)) {
-            return Objects.equals(pet.getOwner().getId(), userId);
+        if (!petRepository.existsById(petId)) {
+            throw new NotFoundLocalizedException(NotFoundLocalizedError.PET_DOES_NOT_EXIST);
         }
 
-        return familyService.familyHasMember(family.getId(), userId);
+        return petRepository.petIsAssociatedWithUser(petId, userId);
     }
 
     public List<Pet> getUserPets(UUID userId) {
-        return petRepository.findAllPetsRelatedToUser(userId);
-    }
-
-    public Pet getPet(UUID userId, UUID petId) throws NotFoundLocalizedException {
-        if (!userIsRelatedToPet(userId, petId)) {
-            throw new ForbiddenLocalizedException(ForbiddenLocalizedError.USER_IS_NOT_RELATED_TO_PET);
+        if (!accountService.accountExists(userId)) {
+            throw new NotFoundLocalizedException(NotFoundLocalizedError.ACCOUNT_DOES_NOT_EXIST);
         }
 
-        return petRepository.findPetById(petId)
-                            .orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.PET_DOES_NOT_EXIST));
+        return petRepository.findAllPetsAssociatedWithUser(userId);
     }
 
-    public Pet createPet(UUID userId, CreatePetRequest petInfo) throws NotFoundLocalizedException, ForbiddenLocalizedException {
-        User owner = userService.getUser(userId);
-        UUID familyId = petInfo.getFamilyId();
-        Family family = null;
-        if (Objects.nonNull(familyId)) {
-            if (!familyService.familyHasMember(familyId, userId)) {
-                throw new ForbiddenLocalizedException(ForbiddenLocalizedError.USER_IS_NOT_FAMILY_MEMBER);
-            }
-
-            family = familyService.getFamily(familyId);
+    public Pet getUserPet(UUID userId, UUID petId) throws NotFoundLocalizedException {
+        if (!userIsAssociatedWithPet(userId, petId)) {
+            throw new NotFoundLocalizedException(NotFoundLocalizedError.USER_DOES_NOT_HAVE_THIS_PET);
         }
 
-        PetCategory petCategory = petCategoryService.getPetCategory(petInfo.getPetCategoryId());
+        return petRepository.findPetById(petId).orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.PET_DOES_NOT_EXIST));
+    }
+
+    public Pet createPet(UUID userId, CreatePetRequest petInfo) throws NotFoundLocalizedException {
+        Account createdBy = accountService.getAccount(userId);
+
+        Integer petCategoryId = petInfo.getPetCategoryId();
+        PetCategory petCategory = Objects.nonNull(petCategoryId) ? petCategoryService.getPetCategory(petCategoryId) : null;
 
         Pet newPet = Pet.builder()
                         .name(petInfo.getName())
-                        .avatarUrl(petInfo.getAvatarUrl())
-                        .owner(owner)
-                        .family(family)
                         .petCategory(petCategory)
+                        .createdBy(createdBy)
                         .build();
 
         return petRepository.save(newPet);
     }
 
-    public List<Pet> getFamilyPets(UUID userId, UUID familyId) throws NotFoundLocalizedException, ForbiddenLocalizedException {
-        if (!familyService.familyHasMember(familyId, userId)) {
-            throw new ForbiddenLocalizedException(ForbiddenLocalizedError.USER_IS_NOT_FAMILY_MEMBER);
-        }
-
-        return petRepository.findAllByFamilyId(familyId);
-    }
-
     public Pet updatePetMedicalCard(UUID userId,
                                     UUID petId,
                                     UpdateMedicalCardRequest medicalCardInfo) throws NotFoundLocalizedException, ForbiddenLocalizedException, ConflictLocalizedException {
-        Pet pet = getPet(userId, petId);
+        Pet pet = getUserPet(userId, petId);
 
         if (Objects.nonNull(medicalCardInfo.getName())) {
             pet.setName(medicalCardInfo.getName());
         }
 
-        if (Objects.nonNull(medicalCardInfo.getAvatarUrl())) {
-            pet.setAvatarUrl(medicalCardInfo.getAvatarUrl());
-        }
-
-        if (Objects.nonNull(medicalCardInfo.getSex())) {
-            pet.setSex(medicalCardInfo.getSex());
+        if (Objects.nonNull(medicalCardInfo.getGender())) {
+            pet.setGender(medicalCardInfo.getGender());
         }
 
         if (Objects.nonNull(medicalCardInfo.getDateOfBirth())) {
             pet.setDateOfBirth(medicalCardInfo.getDateOfBirth());
         }
 
-        if (Objects.nonNull(medicalCardInfo.getIsSterilized())) {
-            pet.setIsSterilized(medicalCardInfo.getIsSterilized());
+        if (Objects.nonNull(medicalCardInfo.getIsSpayedOrNeutered())) {
+            pet.setIsSpayedOrNeutered(medicalCardInfo.getIsSpayedOrNeutered());
         }
 
         if (Objects.nonNull(medicalCardInfo.getChipNumber())) {
@@ -155,14 +126,9 @@ public class PetService {
         return petRepository.save(pet);
     }
 
-    public void addPetToFamily(UUID userId,
-                               UUID petId,
-                               AddPetToFamilyRequest info) throws NotFoundLocalizedException, ForbiddenLocalizedException {
-        Family family = familyService.getUserFamily(userId, info.getFamilyId());
+    public void deleteUserPet(UUID userId, UUID petId) {
+        Pet pet = getUserPet(userId, petId);
 
-        Pet pet = getPet(userId, petId);
-        pet.setFamily(family);
-
-        petRepository.save(pet);
+        petRepository.delete(pet);
     }
 }

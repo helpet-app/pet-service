6fa80b3da31f20e575b3d3444b41c2a0bf10f816
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-11 17:50:26 +0700

    Add AccountService

---
+++ src/main/java/com/helpet/service/pet/service/AccountService.java
@@ -0,0 +1,29 @@
+package com.helpet.service.pet.service;
+
+import com.helpet.exception.NotFoundLocalizedException;
+import com.helpet.service.pet.service.error.NotFoundLocalizedError;
+import com.helpet.service.pet.storage.model.Account;
+import com.helpet.service.pet.storage.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public boolean accountExists(UUID accountId) {
+        return accountRepository.existsById(accountId);
+    }
+
+    public Account getAccount(UUID accountId) throws NotFoundLocalizedException {
+        return accountRepository.findAccountById(accountId)
+                                .orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.ACCOUNT_DOES_NOT_EXIST));
+    }
+}

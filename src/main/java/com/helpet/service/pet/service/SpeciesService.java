819bd26b499e69cdd4f01911e3d54f2505057af9
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-11 17:54:19 +0700

    Update SpeciesService

--- src/main/java/com/helpet/service/pet/service/SpeciesService.java
+++ src/main/java/com/helpet/service/pet/service/SpeciesService.java
@@ -4,10 +4,10 @@ import com.helpet.exception.ConflictLocalizedException;
 import com.helpet.exception.NotFoundLocalizedException;
 import com.helpet.service.pet.service.error.ConflictLocalizedError;
 import com.helpet.service.pet.service.error.NotFoundLocalizedError;
-import com.helpet.service.pet.store.model.PetCategory;
-import com.helpet.service.pet.store.model.Species;
-import com.helpet.service.pet.store.repository.SpeciesRepository;
-import com.helpet.service.pet.web.dto.request.CreateSpeciesRequest;
+import com.helpet.service.pet.storage.model.PetCategory;
+import com.helpet.service.pet.storage.model.Species;
+import com.helpet.service.pet.storage.repository.SpeciesRepository;
+import com.helpet.service.pet.dto.request.CreateSpeciesRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,20 +23,20 @@ public class SpeciesService {
         this.speciesRepository = speciesRepository;
     }
 
-    public Species getSpecies(Integer id) {
+    public Species getSpecies(Integer id) throws NotFoundLocalizedException {
         return speciesRepository.findSpeciesById(id)
-                .orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.SPECIES_DOES_NOT_EXIST));
+                                .orElseThrow(() -> new NotFoundLocalizedException(NotFoundLocalizedError.SPECIES_DOES_NOT_EXIST));
     }
 
     public Page<Species> getCategorySpecies(Integer petCategoryId, String name, Pageable pageable) {
-        return speciesRepository.findAllByPetCategoryIdAndNameContainingIgnoreCase(petCategoryId, name, pageable);
+        return speciesRepository.findAllByPetCategoryIdAndNameContainingIgnoreCaseOrderByName(petCategoryId, name, pageable);
     }
 
     public Species createSpecies(Integer petCategoryId,
                                  CreateSpeciesRequest speciesInfo) throws NotFoundLocalizedException, ConflictLocalizedException {
         String name = speciesInfo.getName();
 
-        if (speciesRepository.existsByNameAndPetCategoryId(name, petCategoryId)) {
+        if (speciesRepository.existsByNameIgnoreCaseAndPetCategoryId(name, petCategoryId)) {
             throw new ConflictLocalizedException(ConflictLocalizedError.SPECIES_ALREADY_EXISTS);
         }
 

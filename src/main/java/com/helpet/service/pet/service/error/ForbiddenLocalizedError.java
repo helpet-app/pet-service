294704533f97429a2f82f07d7a04c905275c74a6
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-11 17:48:19 +0700

    Update errors

--- src/main/java/com/helpet/service/pet/service/error/ForbiddenLocalizedError.java
+++ src/main/java/com/helpet/service/pet/service/error/ForbiddenLocalizedError.java
@@ -3,8 +3,10 @@ package com.helpet.service.pet.service.error;
 import com.helpet.exception.util.DefaultEnumLocalizedError;
 
 public enum ForbiddenLocalizedError implements DefaultEnumLocalizedError {
-    USER_IS_NOT_RELATED_TO_PET,
-    USER_IS_NOT_FAMILY_MEMBER;
+    ONLY_FAMILY_OWNER_CAN_ADD_MEMBER,
+    ONLY_FAMILY_OWNER_CAN_REMOVE_MEMBER,
+    ONLY_FAMILY_OWNER_CAN_ADD_PET,
+    ONLY_FAMILY_OWNER_CAN_REMOVE_PET;
 
     @Override
     public String getErrorKeyPrefix() {
